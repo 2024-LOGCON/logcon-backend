6f79f4f505de207e7691db4c9fbe1ee9cd038529
PleBea <bini060315kim@gmail.com>
2023-11-15 17:14:39 +0900

    feat: Add admin guard to access notice endpoint

--- src/modules/notice/notice.controller.ts
+++ src/modules/notice/notice.controller.ts
@@ -1,23 +1,35 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { NoticeService } from './notice.service';
 import { CreateNoticeDto } from './dto/create-notice.dto';
 import { UpdateNoticeDto } from './dto/update-notice.dto';
+import { AdminGuard } from '../admin/guards/admin.guard';
 
 @Controller('notice')
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
 
   @Post()
+  @UseGuards(AdminGuard)
   async create(@Body() body: CreateNoticeDto) {
     return await this.noticeService.create(body);
   }
 
   @Patch(':id')
+  @UseGuards(AdminGuard)
   async update(@Param('id') id: string, @Body() body: UpdateNoticeDto) {
     return await this.noticeService.update(id, body);
   }
 
   @Delete(':id')
+  @UseGuards(AdminGuard)
   async delete(@Param('id') id: string) {
     return await this.noticeService.remove(id);
   }
