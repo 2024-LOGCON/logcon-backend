e36af20242fc1137961baa56435ed21cb91d63c8
PleBea <bini060315kim@gmail.com>
2024-01-12 21:03:55 +0900

    feat: Setup docker

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -1,5 +1,3 @@
-import { readFileSync } from 'fs';
-
 const validateEmail = (email: string) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -8,10 +6,10 @@ const validateEmail = (email: string) => {
 
 const validatePassword = (password: string) => password.length >= 8;
 
-const blacklist: {
-  id: string[];
-  name: string[];
-} = JSON.parse(readFileSync('./src/constants/blacklist.json', 'utf-8'));
+const blacklist = {
+  id: ['admin', 'guest'],
+  name: ['관리자', 'admin', 'guest'],
+};
 
 const validateId = (id: string) =>
   !blacklist.id.includes(id) && /[^a-zA-Z0-9]/g.test(id) === false;
